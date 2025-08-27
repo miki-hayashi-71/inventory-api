254de2b82586f8a4e9b220b830325cf137de30b6
miki-hayashi-71 <miki.hayashi.71+work@gmail.com>
2025-08-27 10:04:36 +0900

    refactor: Improve readability of return value

--- src/main/java/com/example/inventory_api/controller/dto/CategoryResponse.java
+++ src/main/java/com/example/inventory_api/controller/dto/CategoryResponse.java
@@ -1,8 +1,12 @@
 package com.example.inventory_api.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryResponse {
     private Integer id;
     private String name;
