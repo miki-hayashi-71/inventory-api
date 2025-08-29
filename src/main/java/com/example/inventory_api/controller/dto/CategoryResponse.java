cb9a375baa0870200bf270c3fa363a214021e4be
Miki <151443210+miki-hayashi-71@users.noreply.github.com>
2025-08-29 13:46:20 +0900

    Revert "カテゴリ編集（PATCH: /categories/{categoryId}）の実装"

--- src/main/java/com/example/inventory_api/controller/dto/CategoryResponse.java
+++ src/main/java/com/example/inventory_api/controller/dto/CategoryResponse.java
@@ -1,6 +1,5 @@
 package com.example.inventory_api.controller.dto;
 
-import com.example.inventory_api.domain.model.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +10,4 @@ import lombok.NoArgsConstructor;
 public class CategoryResponse {
     private Integer id;
     private String name;
-
-    public CategoryResponse(Category category) {
-        this.id = category.getId();
-        this.name = category.getName();
-    }
 }
